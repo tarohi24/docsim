95a2f2dd06a022271a10c251672dd4dd7b0a6092
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-09-11 20:22:51 +0900

    add text module

---
+++ docsim/text.py
@@ -0,0 +1,53 @@
+from collections import Counter
+import re
+from typing import Callable, List
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+class Filter:
+    def apply(self, tokens: List[str]) -> List[str]:
+        raise NotImplementedError('This is an abstract class')
+    
+    def __call__(self, tokens: List[str]) -> List[str]:
+        return self.apply(tokens)
+
+
+class LowerFilter(Filter):
+    def apply(self, tokens: List[str]) -> List[str]:
+        return [w.lower() for w in tokens]
+
+@dataclass
+class StopWordRemover(Filter):
+    stop_words: Set[str] = set(stopwords.words('english'))
+
+    def apply(self, tokens: List[str]) -> List[str]:
+        return [w for w in tokens if w not in self.stop_words]
+
+
+@dataclass
+class RegexRemover(Filter):
+    regex: re.Pattern
+
+    def apply(self, tokens: List[str]) -> List[str]:
+        return [w for w in [re.sub(regex, '', w) for w in tokens] if w != '']
+
+
+class TextProcessor:
+    text: str
+    filters: List[Filter] = [LowerFilter, StopWordRemover, RegexRemover]
+
+    def run(self) -> List[str]:
+
+        def apply_filter(tokens: List[str],
+                         filters: List[FilterType]) -> List[str]:
+            if len(filters) == 0:
+                return tokens
+            else:
+                head, *tail = filters
+                head_applied: List[str] head(tokens)
+                return apply_filter(head_applied, tail)
+
+        tokenized: List[str] = word_tokenize(self.text)
+        return apply_filter(tokenized, self.filters)
