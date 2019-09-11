5de39c2e0e729e2d339d0b41ab160057039d995c
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-09-11 21:16:08 +0900

    add Searcher (algorithm base

---
+++ docsim/ir/base.py
@@ -0,0 +1,46 @@
+"""
+Base class definition
+"""
+from dataclasses import dataclass
+import logging
+import traceback
+
+from docsim.doc_models import Docuemnt
+from docsim.rank import RankItem, TRECConverter
+
+
+logger = logging.getLogger(__file__)
+T = TypeVar('T')
+
+
+def ignore_exception(func: Callable[..., T]) -> Optional[T]:
+    def wrapper(*args, **kwargs):
+        try:
+            val: T = func(*args, **kwargs)
+            return val
+        except Exception as e:
+            logger.error(e, exc_info=True)
+            return
+
+
+@dataclass
+class Searcher:
+    dataset: Dataset
+    queries: List[Document]
+    runname: str
+
+    @ignore_exception
+    def retrieve(self, query: Document) -> RankItem:
+        raise NotImplementedError('This is an abstract class.')
+        
+    def run(self) -> None:
+        items: List[RankItem] = [res
+                                 for res
+                                 in [retrieve(query) for self.queries]
+                                 if res is not None]
+        trec: TRECConverter = TRECConverter(
+            items=items,
+            dataset=self.dataset,
+            runname=self.runname,
+            is_ground_truth=false)
+        trec.dump()
