e6b61646862981ef7292702097636734b4ff3535
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-09-16 10:36:49 +0900

    merge rank.py into ir/base.py

--- docsim/ir/base.py
+++ docsim/ir/base.py
@@ -52,3 +52,55 @@ class Searcher:
             runname=self.runname,
             is_ground_truth=False)
         trec.dump()
+
+
+@dataclass
+class RankItem:
+    """
+    both a prediction result or a ground truth
+    recall, precision and ap considere self as a ground truth
+    """
+    query_id: DocumentID
+    scores: Dict[DocumentID, Real]
+
+    def get_ranks(self) -> List[DocumentID]:
+        return [docid for docid, _ in sorted(self.scores.items(),
+                                             key=itemgetter(1))]
+
+
+@dataclass
+class TRECConverter:
+    dataset: Dataset
+    runname: str
+    is_ground_truth: bool = False
+
+    def get_fpath(self) -> Path:
+        ext: str = 'qrel' if self.is_ground_truth else 'prel'
+        return self.dataset.get_result_dir().joinpath(f'{self.runname}.{ext}')
+
+    def format(self,
+               items: Iterable[RankItem]) -> List[Tuple[str, ...]]:
+        """
+        Convert items to TREC-eval input format
+        """
+        return list(
+            flatten([
+                [
+                    (str(item.query_id), docid, str(score), self.runname)
+                    for docid, score
+                    in sorted(item.scores.items(), key=itemgetter(1), reverse=True)
+                ]
+                for item in items
+            ])
+        )
+
+    def dump(self,
+             items: Iterable[RankItem],
+             ignore_existence: bool = False) -> None:
+        records: List[Tuple[str, ...]] = self.format(items)
+        fpath: Path = self.get_fpath()
+        if not ignore_existence:
+            if fpath.exists():
+                raise AssertionError(f'File exists. {fpath}')
+        with open(self.get_fpath(), 'w') as fout:
+            fout.write('\n'.join(['\t'.join(rec) for rec in records]))
