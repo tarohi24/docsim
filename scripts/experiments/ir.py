43e18a23805376786215a8d5666d40a695e1a556
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-09-16 21:50:10 +0900

    add ir script

---
+++ scripts/experiments/ir.py
@@ -0,0 +1,20 @@
+"""
+Standalone script for IR Experiment
+"""
+from typing import Dict, Type
+
+from docsim.ir.base import Searcher
+from docsim.ir import methods
+from docsim.ir.models import QueryDataset
+
+
+searcher_classes: Dict[str, Type[Searcher]] = {
+    'keyword': methods.keyword.KeywordBaseline,
+}
+
+def main(ds_name: str, runname: str) -> None:
+    query_dataset: QueryDataset = QueryDataset.load_dump(name=ds_name)
+    searcher_cls: Type[Searcher] = searcher_classes[runname]
+    searcher: Searcher = searcher_cls(query_dataset=query_dataset,
+                                      runname=runname)
+    searcher.run()
