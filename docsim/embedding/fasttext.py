a17666c34af2a9c9e9594b46f123be015ad8b382
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-09-17 17:24:11 +0900

    #4 add embed method

--- docsim/embedding/fasttext.py
+++ docsim/embedding/fasttext.py
@@ -10,3 +10,7 @@ from docsim.settings import project_root
 class Fasttext(Model):
     model = fasttext.load_model(
         str(project_root.joinpath('models/fasttext/wiki.en.bin').resolve()))
+
+    @return_vector
+    def embed_paragraph(cls, para: str) -> np.ndarray:
+        return self.model.get_sentence_vector(para)
