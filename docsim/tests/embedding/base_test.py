5b6ec5af576b9a7ecaa3cbe837577afcecab6ca3
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-09-20 18:48:26 +0900

    add valid cases

--- docsim/tests/embedding/base_test.py
+++ docsim/tests/embedding/base_test.py
@@ -7,6 +7,14 @@ from docsim.tests.test_case import DocsimTestCase
 
 class EmbeddingBaseTest(DocsimTestCase):
 
+    def __init__(self, *args, **kwargs):
+        super(EmbeddingBaseTest, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        super(EmbeddingBaseTest, self).setUp()
+        self.vec: np.ndarray = np.arange(6)
+        self.mat: np.ndarray = np.arange(6).reshape(2, 3)
+
     @return_vector
     def identity_vec_mapping(self, x: np.ndarray):
         return x
@@ -17,8 +25,17 @@ class EmbeddingBaseTest(DocsimTestCase):
 
     def test_return_vector(self):
         with pytest.raises(InvalidShape):
-            self.identity_vec_mapping(np.arange(6).reshape(2, 3))
+            self.identity_vec_mapping(self.mat)
+
+        np.testing.assert_array_equal(
+            self.identity_vec_mapping(self.vec),
+            self.vec)
+        
 
     def test_return_mat(self):
         with pytest.raises(InvalidShape):
-            self.identity_mat_mapping(np.arange(6))
+            self.identity_mat_mapping(self.vec)
+
+        np.testing.assert_array_equal(
+            self.identity_mat_mapping(self.mat),
+            self.mat)
