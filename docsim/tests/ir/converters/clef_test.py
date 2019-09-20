8b0ff598113ce656ee6d8189ba071e159413f84c
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-09-20 11:13:19 +0900

    add clef converter tests

---
+++ docsim/tests/ir/converters/clef_test.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import unittest
+from typing import List
+import xml.etree.ElementTree as ET
+
+from docsim.ir.converters.clef import CLEFConverter
+from docsim.settings import project_root
+
+
+class CLEFConverterTest(unittest.TestCase):
+
+    def to_xml_root(self, docid: str) -> ET.Element:
+        """
+        Parameters
+        ----------
+        docid
+            EP... *with* '-'
+            e.g. EP-0050001-A2
+        """
+        first: str = docid[3]
+        second: str = docid[4:6]
+        third: str = docid[6:8]
+        forth: str = docid[8:10]
+
+        fpath: Path = project_root.joinpath(
+            f'data/clef/orig/collection/00000{first}/{second}/{third}/{forth}/{docid}.xml')
+
+        root: ET.Element = ET.parse(str(fpath.resolve())).getroot()
+        return root
+
+    def setUp(self):
+        self.converter: CLEFConverter = CLEFConverter()
+        self.roots: List[ET.Element] = [
+            self.to_xml_root('EP-0050001-A2'),
+        ]
+
+    def test_get_title(self):
+        titles = [
+            'A golf aid.',
+        ]
+        self.assertListEqual(
+            titles,
+            [self.converter._get_title(root) for root in self.roots])
