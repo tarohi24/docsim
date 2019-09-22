75479413fbc2a0d4db897cc414178a52fe0c42d0
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-09-22 12:45:19 +0900

    prepare for all the classes to be implemented

--- docsim/ir/converters/ntcir.py
+++ docsim/ir/converters/ntcir.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from typing import List
+
+from docsim.ir.converters import base
 
 
 @dataclass
-class NTCIRConverter(Converter):
+class NTCIRConverter(base.Converter):
     def _get_docid(self,
                    root: ET.Element) -> str:
         pass
@@ -13,6 +16,12 @@ class NTCIRConverter(Converter):
 
     def _get_title(self,
                    root: ET.Element) -> str:
+        pass
 
     def _get_text(self,
                   root: ET.Element) -> str:
+        pass
+
+    def _get_paragraph_list(self,
+                            root: ET.Element) -> List[str]:
+        pass
