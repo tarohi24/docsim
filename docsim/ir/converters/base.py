0201ba14a1b99061dcee7814348edd3abeb35fbd
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-09-22 12:38:54 +0900

    move to_* to base (because they can be generalized)

--- docsim/ir/converters/base.py
+++ docsim/ir/converters/base.py
@@ -37,11 +37,78 @@ class Converter:
     """
     convert something into IRBase
     """
-    def to_document(self, fpath: Path) -> Iterable[ColDocument]:
-        raise NotImplementedError('This is an abstract class.')
+    def _get_docid(self,
+                   root: ET.Element) -> str:
+        raise NotImplementedError('This is an abstract function')
 
-    def to_paragraph(self, fpath: Path) -> Iterable[ColParagraph]:
-        raise NotImplementedError('This is an abstract class.')
+    def _get_tags(self,
+                  root: ET.Element) -> List[str]:
+        raise NotImplementedError('This is an abstract function')
 
-    def to_query_dump(self, fpath: Path) -> List[QueryDocument]:
-        raise NotImplementedError('This is an abstract class.')
+    def _get_title(self,
+                   root: ET.Element) -> str:
+        raise NotImplementedError('This is an abstract function')
+
+    def _get_text(self,
+                  root: ET.Element) -> str:
+        raise NotImplementedError('This is an abstract function')
+
+    def _get_paragraph_list(self,
+                            root: ET.Element) -> List[str]:
+
+    def to_document(self,
+                    fpath: Path) -> List[ColDocument]:
+        root: ET.Element = ET.parse(str(fpath.resolve())).getroot()
+
+        docid: str = self._get_docid(root)
+        tags: List[str] = self._get_tags(root)
+        title: str = self._get_title(root)
+        text: str = self._get_text(root)
+        return [ColDocument(docid=models.KeywordField(docid),
+                            title=models.TextField(title),
+                            text=models.TextField(text),
+                            tags=models.KeywordListField(tags))]
+
+    def to_paragraph(self,
+                     fpath: Path) -> List[ColParagraph]:
+        root: ET.Element = ET.parse(str(fpath.resolve())).getroot()
+        # text
+        try:
+            paras: List[str] = self._get_paragraph_list(root)
+        except Exception as e:
+            logger.warning(e, exc_info=True)
+            logger.warning('Could not find description field in the original XML.')
+        if len(paras) == 0:
+            logger.warning('No paragraphs found.')
+            return []
+
+        docid: str = self._get_docid(root)
+        tags: List[str] = self._get_tags(root)
+
+        return [
+            ColParagraph(docid=models.KeywordField(docid),
+                         paraid=models.IntField(paraid),
+                         text=models.TextField(para),
+                         tags=models.KeywordListField(tags))
+            for paraid, para in enumerate(paras)]
+
+    def to_query_dump(self,
+                      fpath: Path) -> List[QueryDocument]:
+        root: ET.Element = ET.parse(str(fpath.resolve())).getroot()
+
+        try:
+            paras: List[str] = self._get_paragraph_list(root)
+        except Exception as e:
+            logger.warning(e, exc_info=True)
+            return []
+        if len(paras) == 0:
+            logger.warning('No paragraphs found.')
+            return []
+
+        docid: str = self._get_docid(root)
+        tags: List[str] = self._get_tags(root)
+
+        return [
+            QueryDocument(docid=docid,
+                          paras=paras,
+                          tags=tags)]
--- docsim/ir/converters/clef.py
+++ docsim/ir/converters/clef.py
@@ -51,7 +51,7 @@ class CLEFConverter(Converter):
         return desc
 
     def _get_paragraph_list(self,
-                           root: ET.Element) -> List[str]:
+                            root: ET.Element) -> List[str]:
         desc_root: Optional[ET.Element] = root.find("description")
         if desc_root is None:
             raise NoneException('root is not found')
@@ -67,63 +67,6 @@ class CLEFConverter(Converter):
                 li: List[ET.Element] = ps[0].findall('sl/li')
                 return [d.text for d in li if d.text is not None]
         raise CannotSplitText('No condition is matched')
-
-    def to_document(self,
-                    fpath: Path) -> List[ColDocument]:
-        root: ET.Element = ET.parse(str(fpath.resolve())).getroot()
-
-        docid: str = self._get_docid(root)
-        tags: List[str] = self._get_tags(root)
-        title: str = self._get_title(root)
-        text: str = self._get_text(root)
-        return [ColDocument(docid=models.KeywordField(docid),
-                            title=models.TextField(title),
-                            text=models.TextField(text),
-                            tags=models.KeywordListField(tags))]
-
-    def to_paragraph(self,
-                     fpath: Path) -> List[ColParagraph]:
-        root: ET.Element = ET.parse(str(fpath.resolve())).getroot()
-        # text
-        try:
-            paras: List[str] = self._get_paragraph_list(root)
-        except Exception as e:
-            logger.warning(e, exc_info=True)
-            logger.warning('Could not find description field in the original XML.')
-        if len(paras) == 0:
-            logger.warning('No paragraphs found.')
-            return []
-
-        docid: str = self._get_docid(root)
-        tags: List[str] = self._get_tags(root)
-
-        return [
-            ColParagraph(docid=models.KeywordField(docid),
-                         paraid=models.IntField(paraid),
-                         text=models.TextField(para),
-                         tags=models.KeywordListField(tags))
-            for paraid, para in enumerate(paras)]
-
-    def to_query_dump(self,
-                      fpath: Path) -> List[QueryDocument]:
-        root: ET.Element = ET.parse(str(fpath.resolve())).getroot()
-
-        try:
-            paras: List[str] = self._get_paragraph_list(root)
-        except Exception as e:
-            logger.warning(e, exc_info=True)
-            return []
-        if len(paras) == 0:
-            logger.warning('No paragraphs found.')
-            return []
-
-        docid: str = self._get_docid(root)
-        tags: List[str] = self._get_tags(root)
-
-        return [
-            QueryDocument(docid=docid,
-                          paras=paras,
-                          tags=tags)]
 
 
 if __name__ == '__main__':
