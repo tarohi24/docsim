cb1484a9d6e162612db2d892ca78c74cd1dfd232
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-10-03 20:42:47 +0900

    Add uniq method

--- docsim/utils.py
+++ docsim/utils.py
@@ -1,9 +1,10 @@
 import logging
-from typing import Callable, Optional, Tuple, Type, TypeVar
+from typing import Callable, Hashable, List, Optional, Tuple, Type, TypeVar
 
 
 T = TypeVar('T')
 logger = logging.getLogger(__file__)
+T_has = TypeVar('T_has', bound=Hashable)
 
 
 def ignore_exception(func: Callable[..., T],
@@ -19,3 +20,16 @@ def ignore_exception(func: Callable[..., T],
             logger.error(e, exc_info=True)
             return None
     return wrapper
+
+
+def uniq(lst: List[T_has],
+         orig: List[T_has],
+         n_top: int) -> List[T_has]:
+    if len(lst) == n_top or len(orig) == 0:
+        return lst
+    else:
+        head, *tail = orig
+        if head in lst:
+            return uniq(lst, tail, n_top)
+        else:
+            return uniq([head] + lst, tail, n_top)
