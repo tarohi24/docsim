530807f0af96202a58624db6176bb85f112bedf8
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-10-03 20:57:42 +0900

    Add tests for new Method #51

--- docsim/tests/trec_test.py
+++ docsim/tests/trec_test.py
@@ -1,22 +1,31 @@
 import unittest
-from typing import Dict
+from typing import Dict, Tuple
 
 from docsim.models import RankItem
 
 
 class TestRankItem(unittest.TestCase):
 
-    def setUp(self):
-        super(TestRankItem, self).setUp()
-        self.dummy_score: Dict[str, float] = {
-            'ABC': 33,
-            'TREC': 12,
+    def __init__(self, *args, **kwargs):
+        super(TestRankItem, self).__init__(*args, **kwargs)
+        self.dummy_scores: Dict[Tuple[str, str], float] = {
+            # (docid, tag): score
+            ('EP1000', 'A'): 33.0,
+            ('EP1001', 'A'): 33.0,
+            ('EP1002', 'B'): 20.0,
         }
+        self.query_id: str = 'EPTopic1000'
+        self.ri: RankItem = RankItem(query_id=self.query_id,
+                                     socres=self.dummy_scores)
 
-    def test_get_ranks(self):
-        rankitem: RankItem = RankItem(
-            query_id='dummy',
-            scores=self.dummy_score)
-        self.assertListEqual(
-            ['ABC', 'TREC', ],
-            rankitem.get_ranks())
+    def test_pred_tags_just_n_top(self):
+        pred = self.ri.pred_tag(n_top=2)
+        self.assertListEqual(pred, ['A', 'B'])
+
+    def test_pred_tags_less_n_top(self):
+        pred = self.ri.pred_tag(n_top=1)
+        self.assertListEqual(pred, ['A', ])
+
+    def test_pred_tags_more_n_top(self):
+        pred = self.ri.pred_tag(n_top=3)
+        self.assertListEqual(pred, ['A', 'B'])
