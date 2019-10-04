1e5defae67f800ac62ceab0c4f643e1c7cdea257
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-10-04 14:42:17 +0900

    Avoid duplicate definitions

--- docsim/settings.py
+++ docsim/settings.py
@@ -7,15 +7,13 @@ from elasticsearch import Elasticsearch
 project_root: Path = Path(os.environ['PROJECT_ROOT'])
 
 is_test: bool = bool(int(os.environ['IS_TEST']))
-if is_test:
-    test_dir: Path = project_root.joinpath('docsim/tests')
-    data_dir: Path = test_dir.joinpath('data')
-    models_dir: Path = test_dir.joinpath('models')
-    results_dir: Path = test_dir.joinpath('results')
-else:
-    data_dir: Path = project_root.joinpath('data')  # noqa
-    models_dir: Path = project_root.joinpath('models')  # noqa
-    results_dir: Path = project_root.joinpath('results')  # noqa
+root: Path = test_dir if is_test else project_root
+
+data_dir: Path = root.joinpath('data')
+models_dir: Path = root.joinpath('models')
+results_dir: Path = root.joinpath('results')
+
+test_dir: Path = project_root.joinpath('docsim/tests')
 
 # Elasticsearch
 es: Elasticsearch = Elasticsearch(os.environ['ES_URL'])
