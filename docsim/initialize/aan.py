458f451bbc9ea952e512cbeeb8b434afdc9afed6
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-10-16 15:09:53 +0000

    Add aan.py

---
+++ docsim/initialize/aan.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Dict, Generator, Iterable, List, Type
+
+from docsim.converters.aan import AANConverter
+from docsim.initialize.base import Dataset
+from docsim.settings import data_dir
+
+
+@dataclass
+class AANDataset(Dataset):
+    converter: AANConverter = field(default_factory=AANConverter)
+
+    def iter_orig_files(self) -> Generator[Path, None, None]:
+        return data_dir.joinpath(f'aan/orig/collection').glob(f'*.txt')
+
+    def iter_query_files(self) -> Generator[Path, None, None]:
+        return self.iter_orig_files()
