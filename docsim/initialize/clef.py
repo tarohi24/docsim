11b437d383ce879515349c140e9938b64cc4d365
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-10-29 13:04:00 +0000

    Path objects aren't serializable

--- docsim/initialize/clef.py
+++ docsim/initialize/clef.py
@@ -16,13 +16,14 @@ queries = app.Table('clef_query', default=QueryDocument)
 @app.timer(0.5)
 async def iter_query_files():
     for path in data_dir.joinpath(f'clef/orig/query').glob(f'**/*.xml'):
-        await query_dump.send(value=path)
+        abspath: str = str(path.resolve())
+        await query_dump.send(value=abspath)
 
 
 @app.agent()
-async def query_dump(pathes: faust.Stream[Path]):
+async def query_dump(pathes: faust.Stream[str]):
     async for path in pathes:
-        docs: List[QueryDocument] = converter.to_query_dump(path)
+        docs: List[QueryDocument] = converter.to_query_dump(Path(path))
         for doc in docs:
             queries[doc.docid] = doc
 
