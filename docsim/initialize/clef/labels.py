ec0c4c639622a7df0ac7963f2a205579c0878ca8
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-10-30 03:44:08 +0000

    stopped using faust

--- docsim/initialize/clef/labels.py
+++ docsim/initialize/clef/labels.py
@@ -1,53 +1,59 @@
 """
 Generate labels (qrels, gt)
 """
+from collections import defaultdict
+from dataclasses import dataclass
 from pathlib import Path
 import re
-from typing import Dict, List
+from typing import Dict, Iterable, List
 
-import faust
+from docsim.settings import data_dir
 
 
-app = faust.App('clef_labels', broker='kafka://broker:9092')
 pac_to_docid_mapping: Dict[int, str] = dict()
 topic_pattern = re.compile(r'PAC-(\d+)_EP-(\d+)-([A-Z0-9]{2})')
-qrel_dict = app.Table('clef_labels_qrel', default=list)
+qrel_dict: Dict[str, list] = defaultdict(list)
 
 
 def load_mapping():
-    directory: Path = Path.home().joinpath('clef/topics')
+    directory: Path = Path('/data').joinpath('clef/topics')
     for topic_path in directory.glob('*.xml'):
-        fname: str = topic_path.stem()
+        fname: str = Path(topic_path).stem
         match = topic_pattern.match(fname)
         if match is None:
             raise AssertionError()
         pac_to_docid_mapping[int(match.group(1))] = f'EP{match.group(2)}{match.group(3)}'
 
 
-class Qrel(faust.Record):
+@dataclass
+class Qrel:
     query: str
     rel: str
 
 
-@app.timer(1)
-async def load_original_qrel():
-    path: Path = Path.home().joinpath('clef/PAC-Qrels/PAC_qrels_21_EN.txt')
+def load_original_qrel():
+    path: Path = Path('/data').joinpath('clef/PAC-Qrels/PAC_qrels_21_EN.txt')
     with open(path) as fin:
         lines: List[str] = [line.split(' ') for line in fin.read().splitlines()]
-    async for line in lines:
+    qrels: List[str] = []
+    for line in lines:
         pacid: int = int(line[0][4:])
         docid: str = pac_to_docid_mapping[pacid]
         relid: str = line[2].replace('-', '')
         qrel: Qrel = Qrel(query=docid, rel=relid)
-        await dump_qrels.send(qrel)
+        qrels.append(qrel)
+    dump_qrels.send(value=qrel)
 
 
-@app.agent
-async def dump_qrels(qrels: faust.Stream[Qrel]):
-    async for qrel in qrels:
+def dump_qrels(qrels: Iterable[Qrel]):
+    for qrel in qrels:
         qrel_dict[qrel.query].append(qrel.rel)
 
 
 if __name__ == '__main__':
     load_mapping()
-    app.main()
+
+    with open(data_dir.joinpath('clef/en.qrels'), 'w') as fout:
+        for key, lst in qrel_dict.items():
+            for item in lst:
+                fout.write(f'{key} 1 {item} 1\n')
