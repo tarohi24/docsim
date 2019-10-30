629747cfca07b491b8af2bb994bfcb3639cfc298
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-10-30 12:53:28 +0000

    Add generate corpus

--- data/spm/generate_corpus.py
+++ data/spm/generate_corpus.py
@@ -1,7 +1,33 @@
 """
 Generate corpus for the specific category
 """
-from docsim.elas.searcher import EsSearcher
+import argparse
+from pathlib import Path
+from typing import List
+
+from docsim.elas.search import EsResult, EsSearcher
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset',
+                        type=str,
+                        nargs=1)
+    parser.add_argument('tags',
+                        type=str,
+                        nargs='+')
+    args = parser.parse_args()
+    dataset: str = args.dataset[0]
+    tags: List[str] = args.tags
 
+    res: EsResult = EsSearcher(es_index=dataset)\
+        .initialize_query()\
+        .add_match_all()\
+        .add_filter(terms=tags, field='tags')\
+        .add_source_fields(['text'])\
+        .search()
+
+    path: Path = Path(__file__).parent.joinpath(dataset).joinpath(f'{"-".join(tags)}.txt')
+    with open(path, 'w') as fout:
+        for hit in res.hits:
+            fout.write(hit.source['text'] + '\n')
