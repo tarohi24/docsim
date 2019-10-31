02e39ea7b1406b05d1834338c2483b1886670ff3
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-10-31 11:29:09 +0000

    separate requirements

--- docsim/initialize/clef.py
+++ docsim/initialize/clef.py
@@ -1,10 +1,26 @@
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Generator, Dict
+from typing import Generator, Dict, Type
+
+from typedflow import typedflow as tyf
 
 from docsim.converters.clef import CLEFConverter
 from docsim.initialize.base import Dataset
 from docsim.settings import data_dir
+
+
+@dataclass
+class CollectionLoader(tyf.DataLoader):
+
+    def load(self) -> Generator[tyf.Batch[Path], None, None]:
+        gen: Generator[Path, None, None] = data_dir\
+            .joinpath(f'clef/orig/collection')\
+            .glob(f'**/*.xml')
+        items: List[
+
+
+
+
 
 
 @dataclass
