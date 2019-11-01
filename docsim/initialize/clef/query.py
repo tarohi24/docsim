87cb658cced177e2877c912a6fba9a0f36639f21
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-01 03:27:03 +0000

    Incorporate typedflow

---
+++ docsim/initialize/clef/query.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from typing import Generator, List
+import xml.etree.ElementTree as ET
+
+from typedflow.typedflow import Task, DataLoader, Dumper, Pipeline
+from typedflow.utils import dump_to_one_file
+
+from docsim.converters.clef import CLEFConverter
+from docsim.elas import models
+from docsim.models import ColDocument
+from docsim.settings import data_dir
+
+
+def get_document(path: Path) -> ColDocument:
+    root: ET.Element = ET.parse(str(path.resolve())).getroot()
+    docid: str = converter._get_docid(root)
+    tags: List[str] = converter._get_tags(root)
+    title: str = converter._get_title(root)
+    text: str = converter._get_text(root)
+    return ColDocument(docid=models.KeywordField(docid),
+                       title=models.TextField(title),
+                       text=models.TextField(text),
+                       tags=models.KeywordListField(tags))
+
+
+if __name__ == '__main__':
+    converter: CLEFConverter = CLEFConverter()
+    gen: Generator[Path, None, None] = data_dir\
+        .joinpath(f'clef/orig/query')\
+        .glob(f'*.xml')
+    loader: DataLoader = DataLoader[Path](gen=gen)
+    task: Task = Task[Path, ColDocument](func=get_document)
+    dump_path: Path = data_dir.joinpath('clef/query/dump.bulk')
+    dumper: Dumper = Dumper[ColDocument](
+        func=lambda batch: dump_to_one_file(batch, dump_path))
+    pipeline: Pipeline = Pipeline(
+        loader=loader,
+        pipeline=[task],
+        dumper=dumper)
+    pipeline.run()
