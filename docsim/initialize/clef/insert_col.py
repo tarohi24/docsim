c112a905f540a10e9838fe88c0bc023aa557c469
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-02 11:24:17 +0000

    mod type

--- docsim/initialize/clef/insert_col.py
+++ docsim/initialize/clef/insert_col.py
@@ -16,12 +16,14 @@ from docsim.settings import data_dir, es
 logger = logging.getLogger(__file__)
 
 
-def loading() -> Generator[str, None, None]:
+def loading(index: str) -> Generator[Dict, None, None]:
     directory: Path = data_dir.joinpath(f'clef/dump')
     for path in tqdm(list(directory.glob(f'*.bulk'))):
         with open(path) as fin:
             for line in fin.read().splitlines():
-                yield line
+                dic: Dict = json.loads(line)
+                dic['_index'] = index
+                yield dic
 
 
 if __name__ == '__main__':
@@ -43,7 +45,7 @@ if __name__ == '__main__':
     logger.info(ack)
 
     for ok, response in streaming_bulk(es,
-                                       loading(),
+                                       loading(index=index),
                                        index=index,
                                        chunk_size=100):
         if not ok:
