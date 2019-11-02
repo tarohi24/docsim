ac5f2e2b993eb84c0849a0c52d1be0dc43edcaa5
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-02 07:18:47 +0000

    now gt is executable

--- docsim/initialize/clef/gt.py
+++ docsim/initialize/clef/gt.py
@@ -5,19 +5,27 @@ load original qrel -> map a key to the new key -> dump new qrel
 from dataclasses import dataclass
 from pathlib import Path
 import re
-from typing import Dict, List, Optional, Match, Tuple
+from typing import Dict, Generator, List, Optional, Match, Tuple
+
+from typedflow.typedflow import Task, DataLoader, Dumper, Pipeline
+from typedflow.utils import dump_to_one_file
+from tqdm import tqdm
+
+from docsim.settings import data_dir
 
 
-def loading() -> List[str]:
+def loading() -> Generator[str, None, None]:
     """
     Return
     -----
     e.g. "PAC-1 0 EP-0971326-B1 1"
     """
-    path: Path = Path.home().joinpath('clef/PAC-Qrels/PAC_qrels_21_EN.txt')
+    path: Path = data_dir.joinpath('clef/orig/gt.qrel')
     with open(path) as fin:
         lines: List[str] = fin.read().splitlines()
-    return lines
+    for line in tqdm(lines):
+        yield line
+    return
 
 
 def load_mapping() -> Dict[str, str]:
@@ -31,7 +39,7 @@ def load_mapping() -> Dict[str, str]:
         return (eps.group(1), f'EP{eps.group(2)}{eps.group(3)}')
 
     pac_eps: List[str] = [
-        p.name for p in Path.home().joinpath('clef/topics').glob('PAC-*.xml')]
+        p.name for p in data_dir.joinpath('clef/orig/query/topics').glob('PAC-*.xml')]
     parsed: List[Tuple[str, str]] = [_parse(name) for name in pac_eps]
     dic: Dict[str, str] = {k: v for k, v in parsed}
     return dic
@@ -55,3 +63,19 @@ def parse(line: str,
         target=splitted[2].replace('-', ''),
         relevance=int(splitted[3]))
     return qi
+
+
+if __name__ == '__main__':
+    mapping: Dict[str, str] = load_mapping()
+    gen: Generator[str, None, None] = loading()
+    loader: DataLoader = DataLoader[Path](gen=gen)
+    parse_task: Task = Task[str, QrelItem](
+        func=lambda line: parse(line, mapping=mapping))
+    dump_path: Path = data_dir.joinpath('clef/query/gt.qrel')
+    dumper: Dumper = Dumper[QrelItem](
+        func=lambda batch: dump_to_one_file(batch, dump_path))
+    pipeline: Pipeline = Pipeline(
+        loader=loader,
+        pipeline=[parse_task],
+        dumper=dumper)
+    pipeline.run()
