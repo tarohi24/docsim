904bb2b38fd255d84fc9f27649e3c2d2cb501363
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-04 07:00:58 +0000

    change filters as classes to as functions

--- docsim/utils/text.py
+++ docsim/utils/text.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from dataclasses import dataclass, field
 import re
 from typing import List, Pattern, Set
 
@@ -7,62 +6,25 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 
-class Filter:
-
-    def apply(self, tokens: List[str]) -> List[str]:
-        raise NotImplementedError('This is an abstract class')
-
-    def __call__(self, tokens: List[str]) -> List[str]:
-        return self.apply(tokens)
+pat_token_non_alphabet: Pattern = re.compile('^[^a-z]+$')
+stop_words: Set[str] = set(stopwords.words('english'))
 
 
-class LowerFilter(Filter):
-
-    def apply(self, tokens: List[str]) -> List[str]:
-        return [w.lower() for w in tokens]
+def tokenize(text: str) -> List[str]:
+    return word_tokenize(text)
 
 
-@dataclass
-class StopWordRemover(Filter):
-    stop_words: Set[str] = field(default_factory=lambda: set(stopwords.words('english')))
-
-    def apply(self, tokens: List[str]) -> List[str]:
-        return [w for w in tokens if w not in self.stop_words]
+def remove_stopwords(tokens: List[str]) -> List[str]:
+    return [w for w in tokens if w not in stop_words]
 
 
-@dataclass
-class RegexRemover(Filter):
-    regex: Pattern = field(default_factory=lambda: re.compile('^[^a-zA-Z0-9]+$'))
-
-    def apply(self, tokens: List[str]) -> List[str]:
-        return [w for w in [re.sub(self.regex, '', w) for w in tokens] if w != '']
-
-
-@dataclass
-class TFFilter(Filter):
+def remove_nonalphabet_tokens(tokens: List[str]) -> List[str]:
     """
-    extract top n words
+    it requires `lower` in advance
     """
-    n_words: int
-
-    def apply(self, tokens: List[str]) -> List[str]:
-        return [token for token, _ in Counter(tokens).most_common(self.n_words)]
+    return [w for w in tokens if pat_token_non_alphabet.match(w) is None]
 
 
-@dataclass
-class TextProcessor:
-    filters: List[Filter]
-
-    def apply(self, text: str) -> List[str]:
-
-        def apply_filter(tokens: List[str],
-                         filters: List[Filter]) -> List[str]:
-            if len(filters) == 0:
-                return tokens
-            else:
-                head, *tail = filters
-                head_applied: List[str] = head.apply(tokens)
-                return apply_filter(head_applied, tail)
-
-        tokenized: List[str] = word_tokenize(text)
-        return apply_filter(tokenized, self.filters)
+def extract_toptf_tokens(tokens: List[str],
+                         n_words: int) -> List[str]:
+    return [token for token, _ in Counter(tokens).most_common(n_words)]
