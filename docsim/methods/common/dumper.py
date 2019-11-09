dfa575627d0c4437f8344dcc205db53dca984ef2
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-09 06:56:48 +0000

    impl to_qrel

--- docsim/methods/common/dumper.py
+++ docsim/methods/common/dumper.py
@@ -5,8 +5,18 @@ from docsim.settings import results_dir
 
 
 def Result(TypedDict):
+    """
+    methods related to this item should be declared
+    outside this class because method impl is not allowed by PEP589
+    (at runtime, it's just a dict)
+    """
     query_docid: str
     scores: Dict[str, float]
+
+
+def to_qrel(res: Result) -> str:
+    return '\n'.join([f"{res['query_docid']} 0 {key} {score}"
+                      for key, score in res['scores'].items()])
 
 
 def get_dump_path(dataset: str,
--- docsim/tests/methods/common/test_dumper.py
+++ docsim/tests/methods/common/test_dumper.py
@@ -1,9 +1,23 @@
 from typing import Dict
 
-from docsim.methods.common.dumper import Result, get_dump_path
+from docsim.methods.common.dumper import Result, get_dump_path, to_qrel
 from docsim.settings import results_dir
 
 
 def test_path_func():
     assert get_dump_path(dataset='clef', method='keyword', runname='40')\
         == results_dir.joinpath('clef/keyword/40.prel')
+
+
+def test_to_qrel():
+    scores: Dict[str, float] = {
+        f'EP10{i}': float(i)
+        for i in range(3)
+    }
+    res: Result = {
+        'query_docid': 'EP111',
+        'scores': scores
+    }
+    assert to_qrel(res) == """EP111 0 EP100 0.0
+EP111 0 EP101 1.0
+EP111 0 EP102 2.0"""
