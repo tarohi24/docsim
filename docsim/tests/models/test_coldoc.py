583004dbc6437994ab085db16f477f796440dafe
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-12 13:21:19 +0000

    refactor test_coldoc

--- docsim/tests/models/test_coldoc.py
+++ docsim/tests/models/test_coldoc.py
@@ -3,6 +3,14 @@ import pytest
 from docsim.models import ColDocument
 
 
+@pytest.fixture
+def json_str() -> str:
+    s: str = """
+    {"docid": "EP111", "title": "hello", "text": "hi this is the body", "tags": ["C90B"]}
+    """
+    return s
+
+
 def test_coldoc_init():
     ColDocument(
         docid='EP111',
@@ -12,25 +20,18 @@ def test_coldoc_init():
     )
 
 
-def test_coldoc_from_json():
-    json_str: str = """
-    {"docid": "EP111", "title": "hello", "text": "hi this is the body", "tags": ["C90B"]}
-    """
+def test_coldoc_from_json(json_str):
     ColDocument.from_json(json_str)
 
 
 def test_invalid_coldoc():
-    # docid -> id
-    json_str: str = """
+    s: str = """
     {"id": "EP111", "title": "hello", "text": "hi this is the body", "tags": ["C90B"]}
     """
     with pytest.raises(KeyError):
-        ColDocument.from_json(json_str)
+        ColDocument.from_json(s)
 
 
-def test_docid_access():
-    json_str: str = """
-    {"docid": "EP111", "title": "hello", "text": "hi this is the body", "tags": ["C90B"]}
-    """
+def test_docid_access(json_str):
     doc: ColDocument = ColDocument.from_json(json_str)
     assert doc.docid == 'EP111'
