a36006c5428f5d92ab138dbb3373e51865b5a07e
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-12 03:13:51 +0000

    add a test for checking parse

--- docsim/tests/methods/test_run.py
+++ docsim/tests/methods/test_run.py
@@ -1,14 +1,16 @@
 from pathlib import Path
+from typing import List
 
 import pytest
 
-from docsim.methods.run import get_method
+from docsim.methods.run import get_method, parse
+from docsim.methods.common.methods import M
 from docsim.methods.methods.keywords import KeywordBaseline
 from docsim.settings import param_dir
 
 
 @pytest.fixture
-def sample_yaml_file() -> Path:
+def sample_yaml() -> Path:
     path: Path = param_dir.joinpath('sample.yaml')
     return path
 
@@ -17,3 +19,7 @@ def test_get_method():
     assert get_method('keywords') == KeywordBaseline
     with pytest.raises(KeyError):
         get_method('dummy')
+
+
+def test_parse(sample_yaml):
+    lst: List[M] = parse(sample_yaml)
